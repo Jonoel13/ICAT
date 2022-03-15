6fc07794f19e1b6f6b51a66f64396cb915d40c04
Alexis Tapia <ealexis.t@gmail.com>
2022-03-14 18:57:22 -0600

    style admin

--- resources/views/layouts/adminapp.blade.php
+++ resources/views/layouts/adminapp.blade.php
@@ -24,17 +24,17 @@
   <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" type="text/javascript"></script>
 
   <!-- Vendor CSS Files -->
-  <link rel="stylesheet" href="{{ URL::asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" />
-  <link rel="stylesheet" href="{{ URL::asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" />
-  <link rel="stylesheet" href="{{ URL::asset('assets/vendor/boxicons/css/boxicons.min.css') }}" />
-  <link rel="stylesheet" href="{{ URL::asset('assets/vendor/quill/quill.snow.css') }}" />
-  <link rel="stylesheet" href="{{ URL::asset('assets/vendor/quill/quill.bubble.css') }}" />
-  <link rel="stylesheet" href="{{ URL::asset('assets/vendor/remixicon/remixicon.css') }}" />
-  <link rel="stylesheet" href="{{ URL::asset('assets/vendor/simple-datatables/style.css') }}" />
+  <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/vendor/bootstrap-icons/bootstrap-icons.css'" />
+  <link rel="stylesheet" href="assets/vendor/boxicons/css/boxicons.min.css" />
+  <link rel="stylesheet" href="assets/vendor/quill/quill.snow.css" />
+  <link rel="stylesheet" href="assets/vendor/quill/quill.bubble.css" />
+  <link rel="stylesheet" href="assets/vendor/remixicon/remixicon.css" />
+  <link rel="stylesheet" href="assets/vendor/simple-datatables/style.css" />
 
 
   <!-- Template Main CSS File -->
-  <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}" />
+  <link rel="stylesheet" href="assets/css/style.css" />
   <style type="text/css">
     .btn-outline-success{
       color: #ffffff !important;
@@ -660,18 +660,18 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{ URL::asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-  <script src="{{ URL::asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ URL::asset('assets/vendor/chart.js/chart.min.js') }}"></script>
-  <script src="{{ URL::asset('assets/vendor/echarts/echarts.min.js') }}"></script>
-  <script src="{{ URL::asset('assets/vendor/quill/quill.min.js') }}"></script>
-  <script src="{{ URL::asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-  <script src="{{ URL::asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
-  <script src="{{ URL::asset('assets/vendor/php-email-form/validate.js') }}"></script>
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.min.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
 
 
   <!-- Template Main JS File -->
-  <script src="{{ URL::asset('assets/js/main.js') }}"></script>
+  <script src="assets/js/main.js"></script>
 
 
 </body>
