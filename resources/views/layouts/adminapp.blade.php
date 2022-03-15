832d03f7722c87f9fc49f9430e64b9c34eb2d678
Alexis Tapia <ealexis.t@gmail.com>
2022-03-14 19:12:33 -0600

    style admin head fix

--- resources/views/layouts/adminapp.blade.php
+++ resources/views/layouts/adminapp.blade.php
@@ -25,9 +25,9 @@
   <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" type="text/javascript"></script>
 
   <!-- Vendor CSS Files -->
-  <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.css" />
   <link rel="stylesheet" href="assets/vendor/bootstrap-icons/bootstrap-icons.css'" />
-  <link rel="stylesheet" href="assets/vendor/boxicons/css/boxicons.min.css" />
+  <link rel="stylesheet" href="assets/vendor/boxicons/css/boxicons.css" />
   <link rel="stylesheet" href="assets/vendor/quill/quill.snow.css" />
   <link rel="stylesheet" href="assets/vendor/quill/quill.bubble.css" />
   <link rel="stylesheet" href="assets/vendor/remixicon/remixicon.css" />
@@ -661,11 +661,11 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/chart.js/chart.min.js"></script>
-  <script src="assets/vendor/echarts/echarts.min.js"></script>
-  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/apexcharts/apexcharts.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="assets/vendor/chart.js/chart.js"></script>
+  <script src="assets/vendor/echarts/echarts.js"></script>
+  <script src="assets/vendor/quill/quill.js"></script>
   <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
