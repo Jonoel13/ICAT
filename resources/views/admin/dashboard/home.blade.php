49e825e3896cc4f583e1d0f8ec039f07623f8f67
Alexis Tapia <ealexis.t@gmail.com>
2022-03-28 17:47:01 -0600

    admin dashboard

--- resources/views/admin/dashboard/home.blade.php
+++ resources/views/admin/dashboard/home.blade.php
@@ -1,63 +1,52 @@
 @extends('layouts.adminapp')
 @section('content')
-
-<form method="POST" action="{{url('pruebas')}}" autocomplete="off" enctype="multipart/form-data">
-  {{csrf_field() }}
-  <input type="file" name="image">
-
-  <button type="submit" class="btn btn-primary" onclick="this.disabled=true; this.value='Enviando, espere ...'; this.form.submit();">Aceptar</button>
-
-</form>
-
-
-@foreach($files as $file)
-  <img src="{{ asset ('storage/'. $file->path)}}" alt="" class="img-responsive">
-  <a href="{{url('pruebas/delete')}}/1Dtnm60h8uCb3pa3PBCURUKNyYEEzWyENb0VVgf1.png">delete</a>
-  <br>
-@endforeach
-
-
+<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
 <div class="col-lg-6">
   <div class="card">
     <div class="card-body">
       <h5 class="card-title">Servicios</h5>
 
-      <!-- Bar Chart -->
-      <canvas id="barChart" style="max-height: 400px;"></canvas>
-      <script>
-        document.addEventListener("DOMContentLoaded", () => {
-          new Chart(document.querySelector('#barChart'), {
-            type: 'bar',
-            data: {
-              labels: ['Certificación', 'Capacitacitación'],
-              datasets: [{
-                label: 'Total',
-                data: [ <?php $countCer;?>, 59],
-                backgroundColor: [
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(54, 162, 235, 0.2)'
-                ],
-                borderColor: [
-                  'rgb(75, 192, 192)',
-                  'rgb(54, 162, 235)'
-                ],
-                borderWidth: 1
-              }]
-            },
-            options: {
-              scales: {
-                y: {
-                  beginAtZero: true
-                }
-              }
-            }
-          });
-        });
-      </script>
-      <!-- End Bar CHart -->
 
     </div>
   </div>
+
+
+  <canvas id="myChart" width="400" height="400"></canvas>
+<script>
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Certificaciones'],
+        datasets: [{
+            label: 'servicios',
+            data: [<?php echo $countCer;?>],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+</script>
 </div>
 
 @endsection
