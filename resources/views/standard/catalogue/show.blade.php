9d388d1aabae7266077d4a5bbe325f1c38a85b2c
Alexis Tapia <ealexis.t@gmail.com>
2022-09-23 17:05:50 -0500

    view standard page

--- resources/views/standard/catalogue/show.blade.php
+++ resources/views/standard/catalogue/show.blade.php
@@ -1,10 +1,86 @@
 @extends('base')
 @section('content')
+<style type="text/css">
+
+
+  .breadcrumb{
+    background-color: #ffffff !important;
+  }
+
+  .header-standard{
+    background-image: url("{{ asset('storage/standard')}}/{!! Helper::standarImageId($group->id_standard) !!}");
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 500px;
+    background-size: cover;
+  }
+  .header-body{
+    background-color:rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+
+  }
+  .header-body-text{
+    color: #ffffff;
+    font-weight: 400;
+    font-size: 2.5rem;
+  }
+  .header-body-desc{
+    text-align: center;
+    padding-top: 200px;
+
+  }
+  .header-title{
+    font-size: 1.5rem;
+    color: #9f2241;
+  }
+  .standard-panel{
+    
+    padding: 40px;
+
+  }
+  .standard-fields{
+    background: #f6f6f6;
+    width: 97%;
+    padding: 20px ;
+  }
+  .standard-description{
+    margin-bottom: 40px;
+  }
+  .list-group-item{
+    background:#f6f6f6 !important;
+  }
+  .standard-btn{
+    background: #f6f6f6;
+    height: 250px;
+    width: 100%;
+  }
+  .standard-btn{
+    padding-top: 40px;
+  }
+
+  
+</style>
 <div>
+  <div class="">
+
+    <div class="header-standard">
+      <div class="header-body">
+        <div class="header-body-desc">
+          <p class=" text-b header-title">Estandar</p>
+          <p class="header-body-text">{{$group->group_shortname}}</p>
+        </div>
+        
+      </div>
+    </div>
+    
+
+  </div>
+  <hr>
   <div class="row">
     <nav>
       <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="{{ url('catalogo/certificaciones') }}">Oferte de certificaciones</a></li>
+        <li class="breadcrumb-item"><a class="text-success" href="{{ url('catalogo/certificaciones') }}">Oferte de certificaciones</a></li>
         <li class="breadcrumb-item active">{{$group->group_shortname}}</li>
       </ol>
     </nav>
@@ -12,27 +88,30 @@
 
   <div class="row">
 
-    <div class="col-md-8">
-        <div class="">
-            <img class="img-responsive col-md-6" src="{{ asset('storage/standard')}}/{!! Helper::standarImageId($group->id_standard) !!}">
-        </div>
+    <div class="col-md-8 standard-panel">
+  
 
         <div class="standard-title">
-          {{$group->group_shortname}}
+          <h2>{{$group->group_shortname}}</h2>
         </div>
 
-        <div class="">
+        <div class="text-justify standard-description">
             {!! Helper::standarDescriptionId($group->id_standard) !!}
         </div>
         <div class="">
-            <a class="btn btn-success" href="{{$group->group_documentation}}" target="_blank">Documentación del estándar </a>
+            
 
           @if (Route::has('login'))
 
                   @auth
+                  <div class="standard-btn text-center">
+                      <h3>Opciones de inscripción</h3>
+                      <a class="btn btn-success" href="{{$group->group_documentation}}" target="_blank">Documentación del estándar </a>
                       <a type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
                         Inscribirme
                       </a>
+                  </div>
+                      
 
                   <!-- Modal -->
                   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -75,17 +154,25 @@
     </div>
 
     <div class="col-md-4">
-      <p>
-        Costo de evaluación: ${{$group->group_price}}.00
-        <br>
-        Costo de emisión de ceretificado: $200.00
-        <br>
-        Calificación minima: {{$group->group_min_grade}}%
-        <br>
-        Horas que puede tardad la evaluación: {{$group->group_hours}}
-      </p>
-    </div>
+      <div class="standard-fields">
 
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                  Costo de evaluación: ${{$group->group_price}}.00
+              </li>
+              <li class="list-group-item">
+                  Costo de emisión de ceretificado: $200.00
+              </li>
+              <li class="list-group-item">
+                  Calificación minima: {{$group->group_min_grade}}%
+              </li>
+              <li class="list-group-item">
+                  Horas que puede tardad la evaluación: {{$group->group_hours}}
+              </li>
+          </ul>
+
+      </div>
+    </div>
   </div>
 
 
